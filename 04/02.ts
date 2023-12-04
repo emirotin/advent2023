225a616a06fba3ee1756c7cd165ed05153142f56
Eugene Mirotin <emirotin@gmail.com>
2023-12-04 13:10:26 +0200

    04/02

---
+++ 04/02.ts
@@ -0,0 +1,39 @@
+import { readLines, sum } from "../lib/index.js";
+
+const parseNums = (line: string) => line.split(/\s+/).map((n) => parseInt(n));
+
+const parseLine = (line: string) => {
+	const [_, nums] = line.split(": ");
+	return nums!.trim().split(" | ").map(parseNums);
+};
+
+const cards = readLines(import.meta.url, "input.txt")
+	.map((l) => l.trim())
+	.filter(Boolean)
+	.map(parseLine);
+
+const total = cards.length;
+
+const counts = Array.from({ length: total }).fill(1) as number[];
+
+const countWin = ([winning, draw]: number[][]) => {
+	let res = 0;
+	for (const n of draw!) {
+		if (!winning!.includes(n)) continue;
+
+		res += 1;
+	}
+
+	return res;
+};
+
+for (let i = 0; i < total; i++) {
+	const mult = counts[i]!;
+	const wins = countWin(cards[i]!);
+
+	for (let j = 1; j <= wins; j++) {
+		counts[i + j] += mult;
+	}
+}
+
+console.log(sum(counts));
