fdf8e0ba3da18be83eebf1f4b6b33a04991303ee
Eugene Mirotin <emirotin@gmail.com>
2023-12-09 18:35:25 +0200

    fix typo

--- 09/lib.ts
+++ 09/lib.ts
@@ -1,7 +1,7 @@
 export const getVal = (ns: number[]) => {
 	const cache = new Map<number, Map<number, number>>();
 
-	// is is index, g is generation of derivatives, where 0 is the original sequence
+	// i is index, g is generation of derivatives, where 0 is the original sequence
 	// when last derivative is 0, all of them are 0, by the way the sequences are built
 	// we use memoization here to keep the algo simple while directly expressing the
 	// recursive nature
