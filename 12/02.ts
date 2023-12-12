165fac96827b9f08b3088d771714632e279a0375
Eugene Mirotin <emirotin@gmail.com>
2023-12-12 22:05:04 +0200

    12/02 naive

---
+++ 12/02.ts
@@ -0,0 +1,18 @@
+import { readLines, sum } from "../lib/index.js";
+import { countOptions, parseLine } from "./lib.js";
+
+const repeat = (s: string, n: number, sep: string) =>
+	Array.from({ length: n }).fill(s).join(sep);
+
+const lines = readLines(import.meta.url, "demo.txt")
+	.map((l) => l.trim())
+	.filter(Boolean)
+	.map((line) => {
+		const [row, counts] = line.split(" ");
+		return [repeat(row!, 5, "?"), repeat(counts!, 5, ",")].join(" ");
+	})
+	.map(parseLine);
+
+console.log(
+	sum(lines.map(({ chunks, counts }) => countOptions(chunks, counts)))
+);
