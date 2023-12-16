758d1bb5469a3e695c4e1fa17fc8d334f28607af
Eugene Mirotin <emirotin@gmail.com>
2023-12-16 16:21:50 +0200

    cleanup

--- 16/02.ts
+++ 16/02.ts
@@ -10,26 +10,10 @@ const rows = map.length;
 const cols = map[0]!.length;
 
 const starts = [
-	...Array.from({ length: rows }, (_, r) => ({
-		r,
-		c: -1,
-		d: "e" as const,
-	})),
-	...Array.from({ length: rows }, (_, r) => ({
-		r,
-		c: cols,
-		d: "w" as const,
-	})),
-	...Array.from({ length: cols }, (_, c) => ({
-		r: -1,
-		c,
-		d: "s" as const,
-	})),
-	...Array.from({ length: cols }, (_, c) => ({
-		r: rows,
-		c,
-		d: "n" as const,
-	})),
+	...Array.from({ length: rows }, (_, r) => ({ r, c: -1, d: "e" as const })),
+	...Array.from({ length: rows }, (_, r) => ({ r, c: cols, d: "w" as const })),
+	...Array.from({ length: cols }, (_, c) => ({ r: -1, c, d: "s" as const })),
+	...Array.from({ length: cols }, (_, c) => ({ r: rows, c, d: "n" as const })),
 ];
 
 console.log(
