48e520a190926ce34f98904d8ff33c651451c561
Eugene Mirotin <emirotin@gmail.com>
2023-12-18 00:18:33 +0200

    dijkstra

--- 17/01.ts
+++ 17/01.ts
@@ -1,4 +1,4 @@
-import { readLines, parseNums } from "../lib/index.js";
+import { readLines, parseNums, sum } from "../lib/index.js";
 
 const map = readLines(import.meta.url, "demo.txt")
 	.map((l) => l.trim())
@@ -7,9 +7,34 @@ const map = readLines(import.meta.url, "demo.txt")
 const rows = map.length;
 const cols = map[0]!.length;
 
-const globalHistory = new Map<string, number>();
-
 type Direction = "n" | "s" | "w" | "e" | "x";
+
+type Vertex = {
+	r: number;
+	c: number;
+	d: Direction;
+	s: number;
+};
+
+const directions: Direction[] = ["n", "s", "w", "e"] as const;
+const steps = [1, 2, 3] as const;
+
+const vertexes: Vertex[] = [{ r: 0, c: 0, d: "x", s: 0 }];
+for (let r = 0; r < rows; r++) {
+	for (let c = 0; c < cols; c++) {
+		for (const d of directions) {
+			for (const s of steps) {
+				if (
+					(d === "w" && c < cols - 1) ||
+					(d === "e" && c > 0) ||
+					(d === "n" && r < rows - 1) ||
+					(d === "s" && r > 0)
+				)
+					vertexes.push({ r, c, d, s });
+			}
+		}
+	}
+}
 
 const dr: Record<Direction, number> = {
 	n: -1,
@@ -27,25 +52,8 @@ const dc: Record<Direction, number> = {
 	x: Infinity,
 } as const;
 
-const findMinAux = (
-	r: number,
-	c: number,
-	d: Direction,
-	lastStreakLength: number = 0,
-	history: string[] = []
-): number => {
-	history = [...history, `${r}-${c}`];
-
-	const v = map[r]![c]!;
-
-	if (r === rows - 1 && c === cols - 1) {
-		return v;
-	}
-
-	const key = `${d}(${lastStreakLength})=>${r}-${c}`;
-	if (globalHistory.has(key)) {
-		return globalHistory.get(key)!;
-	}
+const neighbors = (v: Vertex): Vertex[] => {
+	const { r, c, d, s } = v;
 
 	const turnDirections = [
 		(d === "w" || d === "e" || d === "x") && r > 0 && "n",
@@ -55,7 +63,7 @@ const findMinAux = (
 	].filter(Boolean) as Direction[];
 
 	const sameDirection =
-		lastStreakLength >= 3
+		s >= 3
 			? []
 			: ([
 					d === "w" && c > 0 && "w",
@@ -64,29 +72,66 @@ const findMinAux = (
 					d === "s" && r < rows - 1 && "s",
 			  ].filter(Boolean) as Direction[]);
 
-	const nextTurns = [
-		...turnDirections.map((d) => [r + dr[d], c + dc[d], d, 1] as const),
-		...sameDirection.map(
-			(d) => [r + dr[d], c + dc[d], d, lastStreakLength + 1] as const
-		),
-	].filter(([r, c]) => !history.includes(`${r}-${c}`));
+	return [
+		...turnDirections.map((d) => ({ r: r + dr[d], c: c + dc[d], d, s: 1 })),
+		...sameDirection.map((d) => ({ r: r + dr[d], c: c + dc[d], d, s: s + 1 })),
+	].map(getVertex);
+};
 
-	if (nextTurns.length === 0) {
-		return Infinity;
+const getVertex = (v: Vertex) =>
+	vertexes.find(
+		(u) => u.r === v.r && u.c === v.c && u.d === v.d && u.s === v.s
+	)!;
+
+const edge = (v1: Vertex, v2: Vertex) => {
+	return map[v2.r]![v2.c]!;
+};
+
+const q: Vertex[] = [];
+const dist = new Map<Vertex, number>();
+const prev = new Map<Vertex, Vertex | undefined>();
+for (const v of vertexes) {
+	dist.set(v, Infinity);
+	prev.set(v, undefined);
+	q.push(v);
+}
+const source = getVertex({ r: 0, c: 0, d: "x", s: 0 });
+dist.set(source, 0);
+
+while (q.length) {
+	const candidates = q
+		.map((v) => [dist.get(v)!, v] as const)
+		.sort(([a], [b]) => a - b);
+	const [d, u] = candidates[0]!;
+	const i = q.indexOf(u);
+	q.splice(i, 1);
+
+	for (const v of neighbors(u)) {
+		if (!q.includes(v)) continue;
+		const alt = d + edge(u, v);
+		if (alt < dist.get(v)!) {
+			dist.set(v, alt);
+			prev.set(v, u);
+		}
+	}
+}
+
+const targets = vertexes.filter((v) => v.r === rows - 1 && v.c === cols - 1);
+
+const paths = targets.map((t) => {
+	const s: Vertex[] = [];
+	let u = t;
+
+	if (prev.get(u) || u === source) {
+		while (u) {
+			s.unshift(u);
+			u = prev.get(u)!;
+		}
 	}
 
-	const res =
-		Math.min(
-			...nextTurns.map(([r, c, d, s]) => findMinAux(r, c, d, s, history))
-		) + v;
+	return s;
+});
 
-	globalHistory.set(key, res);
-
-	return res;
-};
-
-const findMin = (r: number, c: number) => {
-	return findMinAux(r, c, "x") - map[r]![c]!;
-};
-
-console.log(findMin(0, 0));
+console.log(
+	Math.min(...paths.map((p) => sum(p.slice(1).map(({ r, c }) => map[r]![c]!))))
+);
