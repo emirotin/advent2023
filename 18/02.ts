4255a40fe3208c135cb8fefc1fd93c4d64c1226f
Eugene Mirotin <emirotin@gmail.com>
2023-12-21 00:33:43 +0200

    try cuting most outstanding segments

--- 18/02.ts
+++ 18/02.ts
@@ -13,119 +13,193 @@ import {
 let steps = readLines(import.meta.url, "input.txt")
 	.map((l) => l.trim())
 	.filter(Boolean)
-	.map(parseInstr({ newMode: true }));
+	.map(parseInstr({ newMode: false }));
 
 let segments = stepsToSegments(steps);
 
 let result = 0;
 
+const removeUnneededSegments = () => {
+	let changed = false;
+
+	do {
+		// remove empty segments
+		const n = segments.length;
+		segments = segments.filter(({ n }) => n > 0);
+		changed = segments.length < n;
+
+		// merge subsequent segments
+		let i = 0;
+		let diff;
+		while (i < segments.length - 1) {
+			const s1 = segments[i]!;
+			const s2 = segments[(i + 1) % segments.length]!;
+			if (s1.d === s2.d) {
+				s2.n += s1.n;
+				s2.xmin = Math.min(s1.xmin, s2.xmin);
+				s2.xmax = Math.max(s1.xmax, s2.xmax);
+				s2.ymin = Math.min(s1.ymin, s2.ymin);
+				s2.ymax = Math.max(s1.ymax, s2.ymax);
+				segments.splice(i, 1);
+				changed = true;
+			} else if ((diff = areOpposite(s1, s2))) {
+				Object.assign(s2, diff);
+				segments.splice(i, 1);
+				changed = true;
+			} else {
+				i += 1;
+			}
+		}
+	} while (changed);
+};
+
+const cutBump = (i2: number) => {
+	const i1 = (i2 - 1 + segments.length) % segments.length;
+	const i3 = (i2 + 1) % segments.length;
+	const s1 = segments[i1]!;
+	const s2 = segments[i2]!;
+	const s3 = segments[i3]!;
+
+	if (!isRightTurn(s1, s2) || !isRightTurn(s2, s3)) {
+		return false;
+	}
+
+	let closestEdge: number = 0;
+	switch (s2.d) {
+		case "s": {
+			let otherEdges = segments
+				.filter(isNot(s2))
+				.filter(isVert)
+				.filter((s) => intersects([s1.ymin, s3.ymin], [s.ymin, s.ymax]))
+				.filter((s) => s.xmin <= s2.xmin);
+			closestEdge = Math.max(...otherEdges.map(({ xmin }) => xmin));
+			break;
+		}
+		case "n": {
+			let otherEdges = segments
+				.filter(isNot(s2))
+				.filter(isVert)
+				.filter((s) => intersects([s1.ymin, s3.ymin], [s.ymin, s.ymax]))
+				.filter((s) => s.xmin >= s2.xmin);
+			closestEdge = Math.min(...otherEdges.map(({ xmin }) => xmin));
+			break;
+		}
+		case "e": {
+			let otherEdges = segments
+				.filter(isNot(s2))
+				.filter(isHoriz)
+				.filter((s) => intersects([s1.xmin, s3.xmin], [s.xmin, s.xmax]))
+				.filter((s) => s.ymin >= s2.ymin);
+			closestEdge = Math.min(...otherEdges.map(({ ymin }) => ymin));
+			break;
+		}
+		case "w": {
+			let otherEdges = segments
+				.filter(isNot(s2))
+				.filter(isHoriz)
+				.filter((s) => intersects([s1.xmin, s3.xmin], [s.xmin, s.xmax]))
+				.filter((s) => s.ymin <= s2.ymin);
+			closestEdge = Math.max(...otherEdges.map(({ ymin }) => ymin));
+			break;
+		}
+	}
+
+	const m = Math.min(
+		s1.n,
+		s3.n,
+		Math.abs(closestEdge - (isVert(s2) ? s2.xmin : s2.ymin))
+	);
+	if (m == 0) {
+		return false;
+	}
+
+	s1.n -= m;
+	s3.n -= m;
+	result += m * s2.n;
+
+	switch (s2.d) {
+		case "n":
+			s2.xmin += m;
+			s2.xmax += m;
+			break;
+		case "s":
+			s2.xmin -= m;
+			s2.xmax -= m;
+			break;
+		case "w":
+			s2.ymin -= m;
+			s2.ymax -= m;
+			break;
+		case "e":
+			s2.ymin += m;
+			s2.ymax += m;
+			break;
+	}
+
+	removeUnneededSegments();
+
+	return true;
+};
+
+function* findIndicesGen<T>(a: T[], pred: (x: T) => boolean) {
+	for (let i = 0; i < a.length; i++) {
+		if (pred(a[i]!)) yield i;
+	}
+}
+
+const findIndices = <T>(a: T[], pred: (x: T) => boolean) => [
+	...findIndicesGen(a, pred),
+];
+
+const cutOneBump = (indices: number[]) => {
+	for (const i of indices) {
+		if (cutBump(i)) return true;
+	}
+	return false;
+};
+
+const cutNorth = () => {
+	const horizSegments = segments.filter(isHoriz);
+	if (!horizSegments.length) return false;
+
+	const minY = Math.min(...horizSegments.map((s) => s.ymin));
+	const indices = findIndices(segments, (s) => isHoriz(s) && s.ymin === minY);
+	return cutOneBump(indices);
+};
+
+const cutSouth = () => {
+	const horizSegments = segments.filter(isHoriz);
+	if (!horizSegments.length) return false;
+
+	const maxY = Math.max(...horizSegments.map((s) => s.ymax));
+	const indices = findIndices(segments, (s) => isHoriz(s) && s.ymax === maxY);
+	return cutOneBump(indices);
+};
+
+const cutEast = () => {
+	const vertSegments = segments.filter(isVert);
+	if (!vertSegments.length) return false;
+
+	const maxX = Math.max(...vertSegments.map((s) => s.xmax));
+	const indices = findIndices(segments, (s) => isVert(s) && s.xmax === maxX);
+	return cutOneBump(indices);
+};
+
+const cutWest = () => {
+	const vertSegments = segments.filter(isVert);
+	if (!vertSegments.length) return false;
+
+	const minX = Math.min(...vertSegments.map((s) => s.xmin));
+	const indices = findIndices(segments, (s) => isVert(s) && s.xmin === minX);
+	return cutOneBump(indices);
+};
+
 while (segments.length) {
-	// cut extending rects
-	for (let i = 0; i < segments.length; i++) {
-		const s1 = segments[i]!;
-		const s2 = segments[(i + 1) % segments.length]!;
-		const s3 = segments[(i + 2) % segments.length]!;
-		if (!isRightTurn(s1, s2) || !isRightTurn(s2, s3)) {
-			continue;
-		}
-
-		let closestEdge: number = 0;
-		switch (s2.d) {
-			case "s": {
-				let otherEdges = segments
-					.filter(isNot(s2))
-					.filter(isVert)
-					.filter((s) => intersects([s1.ymin, s3.ymin], [s.ymin, s.ymax]))
-					.filter((s) => s.xmin <= s2.xmin);
-				closestEdge = Math.max(...otherEdges.map(({ xmin }) => xmin));
-				break;
-			}
-			case "n": {
-				let otherEdges = segments
-					.filter(isNot(s2))
-					.filter(isVert)
-					.filter((s) => intersects([s1.ymin, s3.ymin], [s.ymin, s.ymax]))
-					.filter((s) => s.xmin >= s2.xmin);
-				closestEdge = Math.min(...otherEdges.map(({ xmin }) => xmin));
-				break;
-			}
-			case "e": {
-				let otherEdges = segments
-					.filter(isNot(s2))
-					.filter(isHoriz)
-					.filter((s) => intersects([s1.xmin, s3.xmin], [s.xmin, s.xmax]))
-					.filter((s) => s.ymin >= s2.ymin);
-				closestEdge = Math.min(...otherEdges.map(({ ymin }) => ymin));
-				break;
-			}
-			case "w": {
-				let otherEdges = segments
-					.filter(isNot(s2))
-					.filter(isHoriz)
-					.filter((s) => intersects([s1.xmin, s3.xmin], [s.xmin, s.xmax]))
-					.filter((s) => s.ymin <= s2.ymin);
-				closestEdge = Math.max(...otherEdges.map(({ ymin }) => ymin));
-				break;
-			}
-		}
-
-		const m = Math.min(
-			s1.n,
-			s3.n,
-			Math.abs(closestEdge - (isVert(s2) ? s2.xmin : s2.ymin))
-		);
-		if (m == 0) {
-			continue;
-		}
-
-		s1.n -= m;
-		s3.n -= m;
-		result += m * s2.n;
-
-		switch (s2.d) {
-			case "n":
-				s2.xmin += m;
-				s2.xmax += m;
-				break;
-			case "s":
-				s2.xmin -= m;
-				s2.xmax -= m;
-				break;
-			case "w":
-				s2.ymin -= m;
-				s2.ymax -= m;
-				break;
-			case "e":
-				s2.ymin += m;
-				s2.ymax += m;
-				break;
-		}
-	}
-
-	// remove empty segments
-	segments = segments.filter(({ n }) => n > 0);
-	// merge subsequent segments
-	let i = 0;
-	let diff;
-	while (i < segments.length - 1) {
-		const s1 = segments[i]!;
-		const s2 = segments[(i + 1) % segments.length]!;
-		if (s1.d === s2.d) {
-			s2.n += s1.n;
-			s2.xmin = Math.min(s1.xmin, s2.xmin);
-			s2.xmax = Math.max(s1.xmax, s2.xmax);
-			s2.ymin = Math.min(s1.ymin, s2.ymin);
-			s2.ymax = Math.max(s1.ymax, s2.ymax);
-			segments.splice(i, 1);
-		} else if ((diff = areOpposite(s1, s2))) {
-			Object.assign(s2, diff);
-			segments.splice(i, 1);
-		} else {
-			i += 1;
-		}
-	}
-	segments = segments.filter(({ n }) => n > 0);
+	cutNorth();
+	cutEast();
+	cutSouth();
+	cutWest();
 }
 
 console.log(result);
-// console.log(segments);
+console.log(segments);
