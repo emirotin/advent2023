d522463cf3fd36e4105712bcb655513c1cbfeb7d
Eugene Mirotin <emirotin@gmail.com>
2023-12-25 00:23:22 +0200

    23/02

--- 23/02.ts
+++ 23/02.ts
@@ -14,76 +14,82 @@ const cols = map[0]!.length;
 const startC = map[0]!.findIndex((c) => c === ".");
 const endC = map[rows - 1]!.findIndex((c) => c === ".");
 
-type Coords = readonly [number, number];
-
-const longestPaths = Array.from({ length: rows }, () =>
-	Array.from({ length: cols })
-) as Array<Array<{ d: number; hs: string[][] } | undefined>>;
-
-longestPaths[0]![startC] = {
-	d: 0,
-	hs: [[]],
-};
-
-const toCheck = [[0, startC] as Coords];
-
-while (toCheck.length) {
-	const coords = toCheck.shift()!;
-
-	const coordsStr = `${coords[0]}-${coords[1]}`;
-
-	const currentRecord = longestPaths[coords[0]]![coords[1]]!;
-	const d = currentRecord.d;
-	const hs = currentRecord.hs.map((h) => h.concat([coordsStr]));
-
-	const nextTo = [
-		[coords[0] - 1, coords[1]] as const,
-		[coords[0] + 1, coords[1]] as const,
-		[coords[0], coords[1] + 1] as const,
-		[coords[0], coords[1] - 1] as const,
-	]
-		.filter(([r, c]) => r >= 0 && r < rows && c >= 0 && c < cols)
-		.filter(([r, c]) => map[r]![c] !== "#")
-		.filter(([r, c]) => {
-			const lp = longestPaths[r]![c];
-			return (
-				lp === undefined ||
-				(lp.d < d + 1 && hs.some((h) => !h.includes(`${r}-${c}`)))
-			);
-		});
-
-	for (const [r, c] of nextTo) {
-		const currentRecord = longestPaths[r]![c];
-		const newD = d + 1;
-		const newHs = hs
-			.filter((h) => !h.includes(`${r}-${c}`))
-			.concat(currentRecord?.d === d + 1 ? currentRecord!.hs : []);
-		longestPaths[r]![c] = {
-			d: newD,
-			hs: newHs,
-		};
+function* getVertexes() {
+	for (let r = 0; r < rows; r++) {
+		for (let c = 0; c < cols; c++) {
+			if (map[r]![c] === ".") yield [r, c];
+		}
 	}
-
-	toCheck.push(...nextTo);
 }
 
-console.log(longestPaths[rows - 1]![endC]!.d);
+const vertexes = [...getVertexes()];
 
-const mapToShow = map
-	.map((row, r) =>
-		row
-			.map((x, c) =>
-				r === 0 && c === startC
-					? "S"
-					: longestPaths[rows - 1]![endC]!.hs![0]!.includes(`${r}-${c}`) ||
-					  (r === rows - 1 && c === endC)
-					? "O"
-					: x
-			)
-			.join("")
-	)
-	.join("\n");
+type Vertex = { id: string; r: number; c: number; ns: Array<[string, number]> };
 
-console.log("#", mapToShow.split("").filter((c) => c === "#").length);
-console.log(".", mapToShow.split("").filter((c) => c === ".").length);
-console.log("O", mapToShow.split("").filter((c) => c === "O").length);
+const vs: Record<string, Vertex> = Object.fromEntries(
+	vertexes.map(([r, c]) => {
+		const id = `${r}-${c}`;
+
+		const neighbors = [
+			[r! - 1, c!] as const,
+			[r! + 1, c!] as const,
+			[r!, c! - 1] as const,
+			[r!, c! + 1] as const,
+		]
+			.filter(([r, c]) => r >= 0 && r < rows && c > 0 && c < cols)
+			.filter(([r, c]) => map[r]![c] !== "#")
+			.map(([r, c]) => `${r}-${c}`);
+
+		return [id, { id, r: r!, c: c!, ns: neighbors.map((id) => [id, 1]) }];
+	})
+);
+
+let res = 0;
+let start = vs[`0-${startC}`]!;
+while (start.ns.length === 1) {
+	res += 1;
+	const oldId = start.id;
+	delete vs[oldId];
+	start = vs[start.ns[0]![0]!]!;
+	start.ns = start.ns.filter(([id]) => id !== oldId);
+}
+
+let end = vs[`${rows - 1}-${endC}`]!;
+while (end.ns.length === 1) {
+	res += 1;
+	const oldId = end.id;
+	delete vs[oldId];
+	end = vs[end.ns[0]![0]!]!;
+	end.ns = end.ns.filter(([id]) => id !== oldId);
+}
+
+let vToRemove: Vertex | undefined;
+while (
+	(vToRemove = Object.values(vs).find(
+		(v) => v.ns.length === 2 && v.id !== start.id && v.id !== end.id
+	))
+) {
+	const n1 = vToRemove.ns[0]!;
+	const n2 = vToRemove.ns[1]!;
+	const d = n1[1] + n2[1];
+	vs[n1[0]]!.ns = vs[n1[0]]!.ns.filter(([id]) => id !== vToRemove!.id).concat([
+		[n2[0], d],
+	]);
+	vs[n2[0]]!.ns = vs[n2[0]]!.ns.filter(([id]) => id !== vToRemove!.id).concat([
+		[n1[0], d],
+	]);
+	delete vs[vToRemove.id];
+}
+
+const longestPath = (endNodeId: string, history: string[]): number => {
+	if (endNodeId === start.id) return 0;
+
+	const comingFrom = vs[endNodeId]!.ns.filter(([id]) => !history.includes(id));
+	const candidates = comingFrom.map(
+		([id, d]) => d + longestPath(id, [...history, endNodeId])
+	);
+
+	return Math.max(...candidates);
+};
+
+console.log(longestPath(end.id, []) + res);
