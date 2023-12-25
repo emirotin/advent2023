3953c45ed6dd27eb083be6fcfe5aef5e5f7c5916
Eugene Mirotin <emirotin@gmail.com>
2023-12-26 01:31:15 +0200

    solve 25/01

--- 25/01.ts
+++ 25/01.ts
@@ -4,8 +4,6 @@ import { isNotUndefined } from "../lib/index.js";
 
 const { vertexes, adjacency } = getGraph(import.meta.url, "input.txt");
 
-const n = vertexes.length;
-
 const neighbors = (v: string) => {
 	const i = vertexes.indexOf(v);
 	if (i < 0) return [];
@@ -14,5 +12,25 @@ const neighbors = (v: string) => {
 	).filter(isNotUndefined);
 };
 
-// obtained via visually exploring the result of pot.py :-P
+// obtained via visually exploring the result of plot.py :-P
 const EDGES_TO_REMOVE = ["vps-pzc", "dph-cvx", "xvk-sgc"];
+
+for (const edge of EDGES_TO_REMOVE) {
+	const [from, to] = edge.split("-");
+	const i = vertexes.indexOf(from!);
+	const j = vertexes.indexOf(to!);
+	adjacency[i]![j] = 0;
+	adjacency[j]![i] = 0;
+}
+
+const n = vertexes.length;
+const vMinus = new Set<string>();
+const checkNext = [vertexes[0]!];
+
+while (checkNext.length) {
+	const v = checkNext.shift()!;
+	vMinus.add(v);
+	checkNext.push(...neighbors(v).filter((u) => !vMinus.has(u)));
+}
+
+console.log(vMinus.size * (n - vMinus.size));
